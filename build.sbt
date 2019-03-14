024240a6f1fd12dd8b56a82c99dac381f3e2aa2e
Yann Bolliger <yann.bolliger@epfl.ch>
2019-03-14 17:39:22 +0100

    Add sbt sample config and first source file.

---
+++ build.sbt
@@ -0,0 +1,8 @@
+name := "g5-parallel-sgd-spark"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.0"
---
+++ src/main/scala/ExampleApp.scala
@@ -0,0 +1,20 @@
+/* SimpleApp.scala */
+import org.apache.spark.sql.SparkSession
+
+object ExampleApp {
+
+  def main(args: Array[String]) {
+    val logFile = "README.md"
+
+    val spark = SparkSession.builder.appName("Simple Application").getOrCreate()
+
+    val logData = spark.read.textFile(logFile).cache()
+
+    val numAs = logData.filter(line => line.contains("a")).count()
+    val numBs = logData.filter(line => line.contains("b")).count()
+
+    println(s"Lines with a: $numAs, Lines with b: $numBs")
+
+    spark.stop()
+  }
+}
